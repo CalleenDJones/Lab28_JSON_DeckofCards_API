03f81f6767ee2e0238817fb9478ec9632934d3d8
Calleen D. Jones <calleendjones@gmail.com>
2018-06-06 19:04:41 -0400

    First Commit

---
+++ Lab28_JSON_DeckofCard_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab28_JSON_DeckofCard_API.MvcApplication" Language="C#" %>
---
+++ Lab28_JSON_DeckofCard_API/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab28_JSON_DeckofCard_API.Startup))]
+namespace Lab28_JSON_DeckofCard_API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
